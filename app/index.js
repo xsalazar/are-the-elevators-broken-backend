c77f5ecc35318a649f35a9e89c84b966841909cf
Xavier Salazar <x.salazar1729@gmail.com>
2025-04-21 17:02:36 -0700

    Add `PUT` endpoint

--- app/index.js
+++ app/index.js
@@ -1,8 +1,13 @@
-const { GetObjectCommand, S3Client } = require("@aws-sdk/client-s3");
+const {
+  GetObjectCommand,
+  PutObjectCommand,
+  S3Client,
+} = require("@aws-sdk/client-s3");
 
 exports.handler = async (event, context) => {
   console.log(JSON.stringify(event));
   const s3 = new S3Client();
+  const dataKey = "data.json";
 
   // Handle GET request for current data
   if (event.requestContext.http.method === "GET") {
@@ -10,7 +15,7 @@ exports.handler = async (event, context) => {
       await s3.send(
         new GetObjectCommand({
           Bucket: process.env.ELEVATORS_DATA_S3_BUCKET,
-          Key: "data.json",
+          Key: dataKey,
         })
       )
     ).Body.transformToString();
@@ -22,5 +27,61 @@ exports.handler = async (event, context) => {
       headers: { "content-type": "application/json" },
       body: data,
     };
+  }
+
+  if (event.body && event.requestContext.http.method === "PUT") {
+    const body = JSON.parse(event.body);
+    const errorResponse = {
+      cookies: [],
+      isBase64Encoded: false,
+      statusCode: 500,
+      headers: {},
+      body: "",
+    };
+
+    if (
+      !Object.keys(body).includes("elevator") &&
+      !Object.keys(body).includes("isBroken")
+    ) {
+      return errorResponse;
+    }
+
+    const elevator = body.elevator;
+    if (elevator !== "north" && elevator !== "south") {
+      return errorResponse;
+    }
+
+    const isBroken = body.isBroken;
+    if (typeof isBroken !== boolean) {
+      return errorResponse;
+    }
+
+    const data = JSON.parse(
+      await (
+        await s3.send(
+          new GetObjectCommand({
+            Bucket: process.env.ELEVATORS_DATA_S3_BUCKET,
+            Key: dataKey,
+          })
+        )
+      ).Body.transformToString()
+    );
+
+    if (elevator === "north") {
+      data.north.isBroken = isBroken;
+      data.north.timestampUpdated = Math.floor(Date.now() / 1000);
+    } else if (elevator === "south") {
+      data.south.isBroken = isBroken;
+      data.south.timestampUpdated = Math.floor(Date.now() / 1000);
+    }
+
+    await s3.send(
+      new PutObjectCommand({
+        Bucket: process.env.ELEVATORS_DATA_S3_BUCKET,
+        Key: dataKey,
+        Body: JSON.stringify(data),
+        ContentType: "application/json",
+      })
+    );
   }
 };
--- terraform/gateway.tf
+++ terraform/gateway.tf
@@ -2,8 +2,8 @@ resource "aws_apigatewayv2_api" "instance" {
   name          = "are-the-elevators-broken-api-gateway"
   protocol_type = "HTTP"
   cors_configuration {
-    allow_origins = ["https://xsalazar.dev", "https://aretheelevatorsbroken.com"]
-    allow_methods = ["GET"]
+    allow_origins = ["https://xsalazar.dev", "https://aretheelevatorsbroken.com", "*"]
+    allow_methods = ["GET", "PUT"]
     allow_headers = ["*"]
   }
 }
@@ -15,9 +15,15 @@ resource "aws_apigatewayv2_integration" "instance" {
   payload_format_version = "2.0"
 }
 
-resource "aws_apigatewayv2_route" "instance" {
+resource "aws_apigatewayv2_route" "get_instance" {
   api_id    = aws_apigatewayv2_api.instance.id
   route_key = "GET /"
+  target    = "integrations/${aws_apigatewayv2_integration.instance.id}"
+}
+
+resource "aws_apigatewayv2_route" "put_instance" {
+  api_id    = aws_apigatewayv2_api.instance.id
+  route_key = "PUT /"
   target    = "integrations/${aws_apigatewayv2_integration.instance.id}"
 }
 
