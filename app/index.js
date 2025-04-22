a37418f9c7927469a5ea2cbdae0efdb3afd1356b
Xavier Salazar <x.salazar1729@gmail.com>
2025-04-21 17:15:50 -0700

    Return updated data after update

--- app/index.js
+++ app/index.js
@@ -83,5 +83,13 @@ exports.handler = async (event, context) => {
         ContentType: "application/json",
       })
     );
+
+    return {
+      cookies: [],
+      isBase64Encoded: false,
+      statusCode: 200,
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify(data),
+    };
   }
 };
